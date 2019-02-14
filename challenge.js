7ae73925eef0e2da72ca5a4af255675175a758f6
cristian-escobedo <42873254+cristian-escobedo@users.noreply.github.com>
2019-02-14 09:06:48 -0600

    Create challenge.js

---
+++ challenge.js
@@ -0,0 +1,23 @@
+var x = 200;
+var velx = 
+
+function setup() {
+	createCanvas(400, 400);
+}
+
+function mostrar() {
+	
+	ellipse(x, 200, 50, 50);
+}
+	function moverX(){
+	x = x + velx;
+	}
+	function botarX(){
+	if (x < 25)}
+	{
+		velx = velx * -1;
+	}
+
+	if (x > 375) {
+		velx = velx * -1;
+	}
